2dbf8e58291514527eafb4ec9f6a588b70625aec
mgarcia39 <mgarcia39@gmail.com>
2021-09-20 09:48:43 +0200

    Matrículas 22

--- admin/matriculas/listado_total.php
+++ admin/matriculas/listado_total.php
@@ -1,7 +1,7 @@
 <?php
 require('../../bootstrap.php');
 
-acl_acceso($_SESSION['cargo'], array(1, 8));
+acl_acceso($_SESSION['cargo'], array(1, 2, 8));
 
 if (file_exists('config.php')) {
 	include('config.php');
