e844ae63eaf00094fb5d102823bff8e76a4f38ae
mgarcia39 <miguel@iesmonterroso.org>
2021-11-04 12:34:25 +0100

    Corrección de errores

--- cuaderno/poner_notas.php
+++ cuaderno/poner_notas.php
@@ -1,71 +1,12 @@
 <?php defined('INTRANET_DIRECTORY') OR exit('No direct script access allowed'); 
 
 // Procesamos los datos
-if ($seleccionar=="1") {
-$curso_orig=$curso;	
-$curso = substr($curso,0,strlen($curso)-1);
-$cursos = explode(", ",$curso);
-$unidad = "";
-foreach($cursos as $unidad)
-{
-	$alumnos = "";
-	foreach ($_POST as $key => $val) {
-		//echo "$key => $val<br>";
-		if (strlen(strstr($key,"select#")) > 0) {
-		$trozos = explode("#",$key);
-		$unid = str_ireplace("_", " ", $trozos[2]);
-		//echo $unidad." : ".$unid."<br>";
-		if ($unidad == $unid) {
-		$alumnos .= $trozos[1].","; 		
-		}
-		}
-		}
-
-
-		$select1 = "select id, curso, alumnos from grupos where profesor = '$profesor' and (asignatura = '$asignatura'  $extra_asig) and curso = '$unidad'";
-		$select0 = mysqli_query($db_con, $select1);
-		$select = mysqli_fetch_array($select0);
-		if(mysqli_num_rows($select0) == "1"){
-			if (!(empty($alumnos))) {
-		$actualiza = "UPDATE grupos SET alumnos = '$alumnos' WHERE id = '$select[0]'";
-		$actua0 = mysqli_query($db_con, $actualiza);
-		$unidades.=$unidad.", ";	
-			}
-		}
-		else{
-			// Eliminamos el grupo del horario si los alumnos son igual a 0.
-  		if ($alumnos=="") {
-  			$vacio=1;
-  			mysqli_query($db_con,"delete from horw where (c_asig = '$asignatura' or c_asig = '$asignatura2') and prof = '$profesor' and a_grupo = '$unidad'");
-  			mysqli_query($db_con,"delete from horw_faltas where (c_asig = '$asignatura'  or c_asig = '$asignatura2')and prof = '$profesor' and a_grupo = '$unidad'");
-  			mysqli_query($db_con,"delete from profesores where materia = (select distinct nombre from asignaturas where codigo = '$asignatura' and abrev not like '%\_%') and profesor = '$profesor' and grupo = '$unidad'");
-  			}  	
-  			else{
-
-  			$ctrl = mysqli_query($db_con,"select distinct codigo from materias where nombre like (select distinct nombre from materias where codigo = '$asignatura' limit 1) and grupo like '$unidad' and abrev not like '%\_%'");
-  			while ($ctrl_asig = mysqli_fetch_array($ctrl)) {
-  					$insert = "insert into grupos (profesor, asignatura, curso, alumnos) values ('$profesor','$ctrl_asig[0]','$unidad', '$alumnos')";
-  					//echo $insert."<br>";
-  					$insert0 = mysqli_query($db_con, $insert);
-  					$unidades.=$unidad.", ";	
-  				}	 							
-  			}
- 		}	
-	}
-		if ($vacio==1) {
- 			$unidades = substr($unidades,0,strlen($unidades)-1);
- 			mysqli_query($db_con,"update notas_cuaderno set curso='$unidades' where curso='$curso_orig' and (asignatura='$asignatura' or asignatura='$asignatura2')");
- 				}	
-}
-
-  	// Borramos datos en casillas de verificaciÃ³n visibles
-  	$contr = mysqli_query($db_con, "select id from notas_cuaderno where profesor = '$profesor' and Tipo like 'Casilla%' and oculto = '0'");
+// Borramos datos en casillas de verificaciÃ³n visibles
+ 	$contr = mysqli_query($db_con, "select id from notas_cuaderno where profesor = '$profesor' and Tipo like 'Casilla%' and oculto = '0' and asignatura='".$_POST['asignatura']."' and curso = '".$_POST['curso']."'");
   	while($control_veri = mysqli_fetch_array($contr)){
-  		//echo "Borramos registro $claveal ==> $id<br />";
   		$borra_veri = "delete from datos  WHERE datos.id = '$control_veri[0]'";
 		$borra1 = mysqli_query($db_con, $borra_veri);
   	}
- 
  
   foreach ($_POST as $key => $val) {
   	// echo "$key --> $val<br />";
