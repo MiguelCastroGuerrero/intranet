e0934b6bd124ee1e1917dec0678a59aee89990b8
Rubén López <rubenlh91@gmail.com>
2021-12-13 09:43:18 +0100

    Nuevos campos en alma

--- xml/jefe/config_alma.php
+++ xml/jefe/config_alma.php
@@ -46,9 +46,14 @@ $campos	= array(
     "Nacionalidad" => "NACIONALIDAD", 
     "Sexo" => "SEXO", 
     "Fecha de matrícula" => "FECHAMATRICULA", 
-    "NºSeg.Social" => "NSEGSOCIAL");
+    "NºSeg.Social" => "NSEGSOCIAL",
+    "¿Padece alguna enfermedad?" => "ENFERMEDAD",
+    "¿Sigue tratamiento?" => "TRATAMIENTO",
+    "¿Alergia a medicamentos?" => "ALERGIAMEDICAMENTOS",
+    "¿Intolerancias alimenticias?" => "INTOLERANCIAS",
+    "Custodia" => "CUSTODIA");
 
-$campos_texto	= '"Alumno/a" => "Alumno/a", "Estado Matrícula" => "ESTADOMATRICULA", "Nº Id. Escolar" => "CLAVEAL", "DNI/Pasaporte" => "DNI", "Dirección" => "DOMICILIO", "Código postal" => "CODPOSTAL", "Localidad de residencia" => "LOCALIDAD", "Fecha de nacimiento" => "FECHA", "Provincia de residencia" => "PROVINCIARESIDENCIA", "Teléfono" => "TELEFONO", "Teléfono personal alumno/a" => "TELEFONOALUMNO", "Teléfono de urgencia" => "TELEFONOURGENCIA", "Correo electrónico personal alumno/a" => "CORREOALUMNO", "Correo Electrónico" => "CORREO", "Curso" => "CURSO", "Nº del expediente del centro" => "NUMEROEXPEDIENTE", "Unidad" => "UNIDAD", "Primer apellido" => "apellido1", "Segundo apellido" => "apellido2", "Nombre" => "NOMBRE", "DNI/Pasaporte Primer tutor" => "DNITUTOR", "Primer apellido Primer tutor" => "PRIMERAPELLIDOTUTOR", "Segundo apellido Primer tutor" => "SEGUNDOAPELLIDOTUTOR", "Nombre Primer tutor" => "NOMBRETUTOR", "Correo Electrónico Primer tutor" => "CORREOTUTOR", "Teléfono Primer tutor" => "TELEFONOTUTOR", "Sexo Primer tutor" => "SEXOPRIMERTUTOR", "DNI/Pasaporte Segundo tutor" => "DNITUTOR2", "Primer apellido Segundo tutor" => "PRIMERAPELLIDOTUTOR2", "Segundo apellido Segundo tutor" => "SEGUNDOAPELLIDOTUTOR2", "Correo Electrónico Segundo tutor" => "CORREOTUTOR2", "Nombre Segundo tutor" => "NOMBRETUTOR2", "Teléfono Segundo tutor" => "TELEFONOTUTOR2", "Sexo Segundo tutor" => "SEXOTUTOR2", "Localidad de nacimiento" => "LOCALIDADNACIMIENTO", "Año de la matrícula" => "ANOMATRICULA", "Nº de matrículas en este curso" => "MATRICULAS", "Observaciones de la matrícula" => "OBSERVACIONES", "Provincia nacimiento" => "PROVINCIANACIMIENTO", "País de nacimiento" => "PAISNACIMIENTO", "Edad a 31/12 del año de matrícula" => "EDAD", "Nacionalidad" => "NACIONALIDAD", "Sexo" => "SEXO", "Fecha de matrícula" => "FECHAMATRICULA", "NºSeg.Social" => "NSEGSOCIAL"';
+$campos_texto	= '"Alumno/a" => "Alumno/a", "Estado Matrícula" => "ESTADOMATRICULA", "Nº Id. Escolar" => "CLAVEAL", "DNI/Pasaporte" => "DNI", "Dirección" => "DOMICILIO", "Código postal" => "CODPOSTAL", "Localidad de residencia" => "LOCALIDAD", "Fecha de nacimiento" => "FECHA", "Provincia de residencia" => "PROVINCIARESIDENCIA", "Teléfono" => "TELEFONO", "Teléfono personal alumno/a" => "TELEFONOALUMNO", "Teléfono de urgencia" => "TELEFONOURGENCIA", "Correo electrónico personal alumno/a" => "CORREOALUMNO", "Correo Electrónico" => "CORREO", "Curso" => "CURSO", "Nº del expediente del centro" => "NUMEROEXPEDIENTE", "Unidad" => "UNIDAD", "Primer apellido" => "apellido1", "Segundo apellido" => "apellido2", "Nombre" => "NOMBRE", "DNI/Pasaporte Primer tutor" => "DNITUTOR", "Primer apellido Primer tutor" => "PRIMERAPELLIDOTUTOR", "Segundo apellido Primer tutor" => "SEGUNDOAPELLIDOTUTOR", "Nombre Primer tutor" => "NOMBRETUTOR", "Correo Electrónico Primer tutor" => "CORREOTUTOR", "Teléfono Primer tutor" => "TELEFONOTUTOR", "Sexo Primer tutor" => "SEXOPRIMERTUTOR", "DNI/Pasaporte Segundo tutor" => "DNITUTOR2", "Primer apellido Segundo tutor" => "PRIMERAPELLIDOTUTOR2", "Segundo apellido Segundo tutor" => "SEGUNDOAPELLIDOTUTOR2", "Correo Electrónico Segundo tutor" => "CORREOTUTOR2", "Nombre Segundo tutor" => "NOMBRETUTOR2", "Teléfono Segundo tutor" => "TELEFONOTUTOR2", "Sexo Segundo tutor" => "SEXOTUTOR2", "Localidad de nacimiento" => "LOCALIDADNACIMIENTO", "Año de la matrícula" => "ANOMATRICULA", "Nº de matrículas en este curso" => "MATRICULAS", "Observaciones de la matrícula" => "OBSERVACIONES", "Provincia nacimiento" => "PROVINCIANACIMIENTO", "País de nacimiento" => "PAISNACIMIENTO", "Edad a 31/12 del año de matrícula" => "EDAD", "Nacionalidad" => "NACIONALIDAD", "Sexo" => "SEXO", "Fecha de matrícula" => "FECHAMATRICULA", "NºSeg.Social" => "NSEGSOCIAL", "¿Padece alguna enfermedad?" => "ENFERMEDAD", "¿Sigue tratamiento?" => "TRATAMIENTO", "¿Alergia a medicamentos?" => "ALERGIAMEDICAMENTOS", "¿Intolerancias alimenticias?" => "INTOLERANCIAS", "Custodia" => "CUSTODIA"';
 
 $alumnos	= 'CREATE TABLE  `alma` (
  `Alumno/a` varchar( 255 ) default NULL ,
@@ -95,7 +100,12 @@ $alumnos	= 'CREATE TABLE  `alma` (
  `NACIONALIDAD` varchar( 32 ) default NULL,
  `SEXO` varchar( 1 ) default NULL ,
  `FECHAMATRICULA` varchar( 255 ) default NULL,
- `NSEGSOCIAL` varchar( 15 ) default NULL,
+ `NSEGSOCIAL` char( 15 ) default NULL,
+ `ENFERMEDAD` char( 2 ) default NULL,
+ `TRATAMIENTO` char( 2 ) default NULL,
+ `ALERGIAMEDICAMENTOS` char( 2 ) default NULL,
+ `INTOLERANCIAS` char( 2 ) default NULL,
+ `CUSTODIA` varchar( 255 ) default NULL,
  PRIMARY KEY (`CLAVEAL`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;';
 
